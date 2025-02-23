ee32060db4b9617f7de9cb2e83b78b0eedba2ec5
derren-austen <derren@bradpaisley.co.uk>
2025-02-23 16:23:54 +0000

    Added the Scalar OpenApi UI

--- back-end/Spotify.Podcasts.Api/Program.cs
+++ back-end/Spotify.Podcasts.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -7,13 +9,20 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/openapi/v1.json")
+            .WithTitle("Spotify Podcasts API")
+            .WithTheme(ScalarTheme.Solarized)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.MapGet("/", () =>
 {
-    return Results.Ok();
+    return Results.Ok("Home again");
 })
 .WithName("Home");
 
